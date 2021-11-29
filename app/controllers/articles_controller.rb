003a2478f3a7574073f174f9c7a5d8a9a2764a93
Scott Edwards <scotteza@gmail.com>
2021-11-29 10:58:24 +0000

    Add article adding page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
@@ -5,5 +7,17 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+  end
+
+  def new; end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    save_result = @article.save
+    if !save_result
+      render plain: @article.errors.full_messages
+    else
+      redirect_to @article
+    end
   end
 end
