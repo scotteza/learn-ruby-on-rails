48766e52782d905d58921c2453ccb9093b80d8e7
Scott Edwards <scotteza@gmail.com>
2021-11-29 11:13:50 +0000

    Render errors on article add fail, render flash message on article add success

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,15 +9,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     save_result = @article.save
-    if !save_result
-      render plain: @article.errors.full_messages
-    else
+    if save_result
+      flash[:notice] = 'Save succeeded'
       redirect_to @article
+    else
+      render 'new'
     end
   end
 end
